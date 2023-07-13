30b5e17ce8177020198f171b0411ca4039d5dd7a
ningyuxibao <134712091+ningyuxibao@users.noreply.github.com>
2023-07-13 19:55:26 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -116,7 +116,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT Ne实际上
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
