a0fef648daae27c84a8335289717103219588f5e
ningyuxibao <134712091+ningyuxibao@users.noreply.github.com>
2023-07-13 19:56:35 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -116,10 +116,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Ne实际上
+          乐迪GPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          完全免费，内涵3.5，4，claude(2，100k，2-100k)
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
